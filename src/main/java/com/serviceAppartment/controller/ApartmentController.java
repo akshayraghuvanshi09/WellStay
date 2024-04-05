771ddc86f2710ba9ebb1990a1e19918bacc57700
Akshay <akshayraghuvanshi09@gmail.com>
2024-04-05 19:00:55 +0530

    first commit

--- src/main/java/com/serviceAppartment/controller/ApartmentController.java
+++ src/main/java/com/serviceAppartment/controller/ApartmentController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.serviceAppartment.dto.ApartmentDTO;
-import com.serviceAppartment.model.Apartment;
 import com.serviceAppartment.service.ApartmentService;
 
 @RestController
